990b5ff5d9af22c1817220f74c501a5f3a1330e4
Karun Kannan <karun.kannan15@gmail.com>
2018-01-24 18:12:44 -0500

    modules are chosen during installation of OpenCV but during configuration of main project

--- opencv/OpenCV.cmake
+++ opencv/OpenCV.cmake
@@ -2,9 +2,13 @@ cmake_minimum_required (VERSION 2.6)
 include(ExternalProject)
 project(opencv-download NONE)
 
-list(APPEND
-    OPENCV_CMAKE_ARGS
-    "-DOPENCV_EXTRA_MODULES_PATH=:opencv_contrib-src/modules"
+set(OPENCV_ARGS "TEST")
+
+message(STATUS, "${OPENCV_ARGS}")
+
+set(
+    OPENCV_ARGS
+    "-DOPENCV_EXTRA_MODULES_PATH:PATH=opencv_contrib-src/modules"
     "-DBUILD_opencv_flann:BOOL=OFF"
     "-DBUILD_opencv_features2d:BOOL=OFF"
     "-DBUILD_opencv_calib3d:BOOL=OFF"
@@ -15,8 +19,10 @@ list(APPEND
     "-DBUILD_opencv_stitching:BOOL=OFF"
     "-DBUILD_opencv_saliency:BOOL=OFF"
     "-DBUILD_opencv_dnn:BOOL=OFF"
+    PARENT_SCOPE
     )
 
+message(STATUS, ${OPENCV_ARGS})
 
 ExternalProject_Add(
     opencv
@@ -25,7 +31,33 @@ ExternalProject_Add(
 
     SOURCE_DIR "${CMAKE_BINARY_DIR}/opencv-src"
     BINARY_DIR "${CMAKE_BINARY_DIR}/opencv-build"
-    CMAKE_ARGS ${OPENCV_CMAKE_ARGS}
+    CMAKE_ARGS
+        "-DBUILD_opencv_apps:BOOL=OFF"
+        "-DBUILD_opencv_calib3d:BOOL=OFF"
+        "-DBUILD_opencv_core:BOOL=ON"
+        "-DBUILD_opencv_dnn:BOOL=OFF"
+        "-DBUILD_opencv_features2d:BOOL=OFF"
+        "-DBUILD_opencv_flann:BOOL=OFF"
+        "-DBUILD_opencv_highgui:BOOL=ON"
+        "-DBUILD_opencv_imgcodecs:BOOL=ON"
+        "-DBUILD_opencv_imgproc:BOOL=OFF"
+        "-DBUILD_opencv_js:BOOL=OFF"
+        "-DBUILD_opencv_ml:BOOL=OFF"
+        "-DBUILD_opencv_objdetect:BOOL=OFF"
+        "-DBUILD_opencv_photo:BOOL=OFF"
+        "-DBUILD_opencv_python2:BOOL=OFF"
+        "-DBUILD_opencv_python3:BOOL=OFF"
+        "-DBUILD_opencv_python_bindings_generator:BOOL=OFF"
+        "-DBUILD_opencv_saliency:BOOL=OFF"
+        "-DBUILD_opencv_shape:BOOL=OFF"
+        "-DBUILD_opencv_stitching:BOOL=OFF"
+        "-DBUILD_opencv_superres:BOOL=OFF"
+        "-DBUILD_opencv_ts:BOOL=OFF"
+        "-DBUILD_opencv_video:BOOL=OFF"
+        "-DBUILD_opencv_videoio:BOOL=OFF"
+        "-DBUILD_opencv_world:BOOL=OFF"
+        "-DBUILD_opencv_xfeatures2d:BOOL=OFF"
+        "-DOPENCV_EXTRA_MODULES_PATH:PATH=opencv_contrib-src/modules"
     )
 #better not to use CMAKE_ARGS
 #define the arguments before using, move the list above
