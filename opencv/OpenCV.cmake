305d901032e40660bbb32a51a8af126bc537fd7d
Karun Kannan <karun.kannan15@gmail.com>
2018-01-26 21:13:45 -0500

    modified to point opencv_dir to build dir

--- opencv/OpenCV.cmake
+++ opencv/OpenCV.cmake
@@ -2,28 +2,6 @@ cmake_minimum_required (VERSION 2.6)
 include(ExternalProject)
 project(opencv-download NONE)
 
-set(OPENCV_ARGS "TEST")
-
-message(STATUS, "${OPENCV_ARGS}")
-
-set(
-    OPENCV_ARGS
-    "-DOPENCV_EXTRA_MODULES_PATH:PATH=opencv_contrib-src/modules"
-    "-DBUILD_opencv_flann:BOOL=OFF"
-    "-DBUILD_opencv_features2d:BOOL=OFF"
-    "-DBUILD_opencv_calib3d:BOOL=OFF"
-    "-DBUILD_opencv_ml:BOOL=OFF"
-    "-DBUILD_opencv_video:BOOL=OFF"
-    "-DBUILD_opencv_shape:BOOL=OFF"
-    "-DBUILD_opencv_xfeatures2d:BOOL=OFF"
-    "-DBUILD_opencv_stitching:BOOL=OFF"
-    "-DBUILD_opencv_saliency:BOOL=OFF"
-    "-DBUILD_opencv_dnn:BOOL=OFF"
-    PARENT_SCOPE
-    )
-
-message(STATUS, ${OPENCV_ARGS})
-
 ExternalProject_Add(
     opencv
     GIT_REPOSITORY https://github.com/opencv/opencv
@@ -40,7 +18,7 @@ ExternalProject_Add(
         "-DBUILD_opencv_flann:BOOL=OFF"
         "-DBUILD_opencv_highgui:BOOL=ON"
         "-DBUILD_opencv_imgcodecs:BOOL=ON"
-        "-DBUILD_opencv_imgproc:BOOL=OFF"
+        "-DBUILD_opencv_imgproc:BOOL=ON"
         "-DBUILD_opencv_js:BOOL=OFF"
         "-DBUILD_opencv_ml:BOOL=OFF"
         "-DBUILD_opencv_objdetect:BOOL=OFF"
@@ -57,7 +35,7 @@ ExternalProject_Add(
         "-DBUILD_opencv_videoio:BOOL=OFF"
         "-DBUILD_opencv_world:BOOL=OFF"
         "-DBUILD_opencv_xfeatures2d:BOOL=OFF"
-        "-DOPENCV_EXTRA_MODULES_PATH:PATH=opencv_contrib-src/modules"
+        "-DOPENCV_EXTRA_MODULES_PATH:PATH=${CMAKE_BINARY_DIR}/opencv_contrib-src/modules"
     )
 #better not to use CMAKE_ARGS
 #define the arguments before using, move the list above
